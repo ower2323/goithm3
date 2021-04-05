97ccfe3d3f720a21fdf023895856d392298af7f3
Your Name <you@example.com>
2021-04-05 19:20:14 +0300

    hw3

---
+++ hw3.py
@@ -0,0 +1,7 @@
+def fibonacci(n):
+    if n in (1, 2):
+        return 1
+    return fibonacci(n - 1) + fibonacci(n - 2)
+if __name__ == "__main__":
+  n = int(input('Введите искомый элемент последовательности ряда Фибоначчи : '))
+print(fibonacci(n))
--- дз.py
+++
@@ -1,8 +0,0 @@
-def fibonacci(n):
-    current = 1
-    if n > 2:
-        current = fibonacci(n-1) + fibonacci(n-2)
-    return current
-if __name__ == "__main__":
-    n = int(input('Введите искомый элемент последовательности ряда Фибоначчи : '))
-print (fibonacci(n))
